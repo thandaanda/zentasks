9d2f65e7acee56a0dc6a9667617c5671f2ed5e0a
Keyur <keyur.raval18@gmail.com>
2014-06-21 00:12:04 +0530

    user management

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,63 @@
 package controllers;
 
+import static play.data.Form.form;
 import models.Project;
 import models.Task;
+import models.User;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import views.html.index;
+import views.html.login;
 
 public class Application extends Controller {
 
+	@Security.Authenticated(Secured.class)
 	public static Result index() {
-		return ok(index.render(Project.projeFinder.all(), Task.findTask.all()));
+		return ok(index.render(
+		        Project.findInvolving(request().username()), 
+		        Task.findToDoInvolving(request().username()),
+		        User.userFinder.byId(request().username())
+		    )); 
+	}
+
+	public static Result login() {
+		return ok(login.render(form(Login.class)));
+	}
+
+	public static class Login {
+		public String email;
+		public String password;
+
+		public String validate() {
+			if (User.authenticate(email, password) == null) {
+				return "Invalid user or password";
+			}
+			return null;
+		}
+	}
+
+	public static Result authenticate() {
+
+		Form<Login> loginForm = form(Login.class).bindFromRequest();
+		if (loginForm.hasErrors()) {
+			return badRequest(login.render(loginForm));
+
+		} else {
+			session().clear();
+			session("email", loginForm.get().email);
+			return redirect(routes.Application.index());
+		}
+
+	}
+	
+	public static Result logout() {
+	    session().clear();
+	    flash("success", "You've been logged out");
+	    return redirect(
+	        routes.Application.login()
+	    );
 	}
 
 }
---
+++ app/controllers/Secured.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.mvc.Http.Context;
+import play.mvc.Result;
+import play.mvc.Security;
+
+public class Secured extends Security.Authenticator{
+	
+	public String getUserName(Context ctx)
+	{
+		return ctx.session().get("email");
+	}
+	
+	@Override
+	public Result onUnauthorized(Context ctx)
+	{
+		return redirect(routes.Application.login());
+	}
+	
+	
+	
+
+}
